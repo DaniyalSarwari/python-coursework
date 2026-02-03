a917a02be0a39d77b683269361de083951f5471c
DaniyalSarwari <iamdaniyalsarwari@gmail.com>
2026-02-03 21:58:52 +0500

    bulk file renamer add docstring

---
+++ projects/bulk_file_renamer.py
@@ -0,0 +1,6 @@
+"""
+Docstring for projects.bulk_file_renamer
+
+    The goal of this project is to build a Bulk File Renamer application that allows users to rename multiple files at once based on a defined naming pattern. \
+    The program should enable users to select a directory and automatically rename all files using rules such as adding a prefix or suffix, numbering files sequentially.
+"""
