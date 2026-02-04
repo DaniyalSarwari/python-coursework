9277690c5c13c9327b5bd84b0b17ec4909379076
DaniyalSarwari <iamdaniyalsarwari@gmail.com>
2026-02-04 23:21:12 +0500

    bulk file renamer project

--- projects/bulk_file_renamer.py
+++ projects/bulk_file_renamer.py
@@ -4,3 +4,44 @@ Docstring for projects.bulk_file_renamer
     The goal of this project is to build a Bulk File Renamer application that allows users to rename multiple files at once based on a defined naming pattern. \
     The program should enable users to select a directory and automatically rename all files using rules such as adding a prefix or suffix, numbering files sequentially.
 """
+import os
+from pathlib import Path
+
+def bulk_renamer(location: Path, prefix: str) -> int:
+    total: int = 0
+    files_list: list[str] = os.listdir(location)
+    # print(files_list)
+    for file in files_list:
+        new_file_name: str
+        file_item: list = file.split(sep='.')
+        file_item[0] = prefix + str(total)
+        new_file_name = '.'.join(file_item)
+
+        source_path: Path = location/file
+        # print(source_path)
+        dest_path: Path = location/new_file_name
+        # print(dest_path)
+
+        source_path.rename(dest_path)
+        total += 1
+    
+    return total
+        
+
+def main():
+    print("===BULK FILE RENAMER===",end='\n\n')
+
+    current: Path = Path.cwd()
+    location: Path = Path(current)/"Files"  # create a folder with name "Files" for files to be placed in it
+    location.mkdir(exist_ok=True)
+
+    status: str = input(f"Please place your all files at ( {location} ) and write 'done': ")
+    input_word: str = input("Please Enter prefix word: ")
+
+    if status == "done":
+        count: int = bulk_renamer(location, input_word)
+        print(f"Total {count} file renamed successfully")
+    
+
+if __name__ == "__main__":
+    main()
