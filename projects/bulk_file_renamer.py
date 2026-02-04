bf024eadadf5e013fa1eab339ec219fff90fd327
DaniyalSarwari <iamdaniyalsarwari@gmail.com>
2026-02-04 23:37:40 +0500

    update docstring

--- projects/bulk_file_renamer.py
+++ projects/bulk_file_renamer.py
@@ -2,7 +2,8 @@
 Docstring for projects.bulk_file_renamer
 
     The goal of this project is to build a Bulk File Renamer application that allows users to rename multiple files at once based on a defined naming pattern. \
-    The program should enable users to select a directory and automatically rename all files using rules such as adding a prefix or suffix, numbering files sequentially.
+    The program should enable users to select a directory, place files in it and automatically rename all files using rules such as adding a prefix or suffix, numbering files sequentially.
+    Location: ./Files  (Files folder in a current working directory)
 """
 import os
 from pathlib import Path
